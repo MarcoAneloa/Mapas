79d735c90b2785265cab7cf86ddcd76074d5c3ab
MarcoAneloa <maneloa89@gmail.com>
2017-01-14 09:23:33 -0500

    actividad de mapas

--- app/src/main/java/com/aneloa/mapas/MainActivity.java
+++ app/src/main/java/com/aneloa/mapas/MainActivity.java
@@ -1,13 +1,26 @@
 package com.aneloa.mapas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button ubicacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    ubicacion=(Button) findViewById(R.id.ubicacion);
+        ubicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
